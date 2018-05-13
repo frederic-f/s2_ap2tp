261e1d9f0bfce7e4c0d2937c0c785b4bfbe3e2db
frederic-f <=>
2018-05-13 16:51:35 +0200

    tp2

--- tp2/02_test_vecteur2d.c
+++ tp2/02_test_vecteur2d.c
@@ -1,3 +1,5 @@
+#include <time.h>
+
 #include "02_vecteur2d.c"
 #include "02_suites.c"
 
@@ -96,6 +98,46 @@ void test_a () {
 
 
 /* *******************************************************************
+ * Q.8 - Temps de calcul
+ ****************************************************************** */
+
+/* Calcul de un selon Q1 */
+
+void calcul_u_param (uint n) {
+    long unsigned int dt;
+    struct timespec start, stop;
+
+    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+    test_suite_u_param (n) ;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
+
+    dt = (stop.tv_sec-start.tv_sec)*1000000000+(stop.tv_nsec-start.tv_nsec);
+    printf("durée écoulée: %lu\n\n", dt);
+}
+
+void calcul_a_param (uint n) {
+    long unsigned int dt;
+    struct timespec start, stop;
+
+    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+    test_a_param (n) ;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
+
+    dt = (stop.tv_sec-start.tv_sec)*1000000000+(stop.tv_nsec-start.tv_nsec);
+    printf("\ndurée écoulée: %lu\n\n", dt);
+}
+
+void comparaison_temps_calcul () {
+    calcul_u_param (10) ;
+    calcul_a_param (10) ;
+
+    calcul_u_param (20) ;
+    calcul_a_param (20) ;
+    calcul_u_param (30) ;
+    calcul_a_param (30) ;
+}
+
+/* *******************************************************************
  * Main
  ****************************************************************** */
 
@@ -112,5 +154,62 @@ int main () {
     printf ("Test a\n") ;
     test_a () ;
 
+
+
+    printf ("\n\n") ;
+    printf ("Temps de calcul \n\n") ;
+    comparaison_temps_calcul () ;
+
     return 0 ;
 }
+
+
+/*
+Test vecteur 2d
+(0.000, 0.000)
+(5.000, 5.000)
+
+
+Test f
+f ( (0.000, 0.000) ) = (0.000, 0.000)
+f ( (5.000, 5.000) ) = (5.000, 5.000)
+f ( (3.000, 2.000) ) = (2.200, 2.600)
+
+
+Test a
+a(0) =(5.000, 1.000)
+u(0) = 5.000000
+v(0) = 1.000000
+
+a(1) =(1.800, 3.400)
+u(1) = 1.800000
+v(1) = 3.400000
+
+a(2) =(3.080, 2.440)
+
+u(2) = 3.080000
+v(2) = 2.440000
+
+
+Temps de calcul
+
+u(10) = 2.714525
+durée écoulée: 39506
+
+a(10) =(2.715, 2.714)
+durée écoulée: 7828
+
+u(20) = 2.714286
+durée écoulée: 11411582
+
+a(20) =(2.714, 2.714)
+durée écoulée: 3005
+
+u(30) = 2.714286
+durée écoulée: 5866137828
+
+a(30) =(2.714, 2.714)
+durée écoulée: 3537
+
+*/
+
